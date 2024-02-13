c0714679570055bdd73c32db7bd3f9dab1fc7aeb
terraform <159118781+sushma933@users.noreply.github.com>
2024-02-13 20:32:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,14 +10,12 @@ resource "aws_subnet" "public_subnet_1" {
   vpc_id            = aws_vpc.example_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
-  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "public_subnet_2" {
   vpc_id            = aws_vpc.example_vpc.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
-  map_public_ip_on_launch = true
 }
 
 resource "aws_route_table" "public_route_table_1" {
@@ -121,4 +119,33 @@ output "public_subnet_1_id" {
 
 output "public_subnet_2_id" {
   value = aws_subnet.public_subnet_2.id
+}
+resource "aws_launch_configuration" "example1" {
+  name = "custom-example-launchconfig"
+  # Ubuntu Server 18.04 LTS (HVM), SSD Volume Type in ap-south-01
+  image_id        = "ami-0c7217cdde317cfec"
+  instance_type   = "t2.micro"
+  associate_public_ip_address = true
+  security_groups = [aws_security_group.public_subnet_sg_1.id, aws_security_group.public_subnet_sg_2.id]
+  user_data = <<-EOF
+              #!/bin/bash
+              echo '<html><body><h1>Hello </h1></html>' > index.html
+              nohup busybox httpd -f -p "${80}" &
+              EOF
+
+  # Whenever using a launch configuration with an auto scaling group, you must set below
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+# Define auto-scaling group
+resource "aws_autoscaling_group" "example" {
+  name                     = "example-asg"
+  launch_configuration    = aws_launch_configuration.example1.name
+  min_size                 = 1
+  max_size                 = 4
+  desired_capacity         = 2
+  vpc_zone_identifier      = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+  health_check_grace_period = 30
+  health_check_type         = "EC2"
 }
